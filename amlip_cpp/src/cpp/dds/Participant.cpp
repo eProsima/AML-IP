5b5e89214d45524e9b15c99a5474f7c69da34470
jparisu <javierparis@eprosima.com>
2022-04-18 11:52:27 +0200

    Refs #14320: Add DdsHandler implementation
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- amlip_cpp/src/cpp/dds/Participant.cpp
+++ amlip_cpp/src/cpp/dds/Participant.cpp
@@ -16,15 +16,7 @@
  * @file Participant.cpp
  */
 
-#include <assert.h>
-
-#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
-
-#include <ddsrouter_utils/exception/InconsistencyException.hpp>
-#include <ddsrouter_utils/exception/InitializationException.hpp>
-
 #include <dds/Participant.hpp>
-#include <types/AmlipGenericTopicDataType.hpp>
 
 namespace eprosima {
 namespace amlip {
@@ -35,81 +27,23 @@ const DomainIdType Participant::DEFAULT_DOMAIN_ID_(166u);
 using namespace eprosima::fastdds::dds;
 
 Participant::Participant(
-        const types::AmlipId& id,
-        const eprosima::fastdds::dds::DomainParticipantQos& qos /* = Participant::default_participant_qos_() */,
-        const DomainIdType& domain /* = Participant::default_domain_id_() */)
+        types::AmlipId id,
+        eprosima::fastdds::dds::DomainParticipantQos qos /* = Participant::default_participant_qos() */,
+        DomainIdType domain /* = Participant::default_domain_id() */)
     : id_(id)
-    , participant_(nullptr)
-    , publisher_(nullptr)
-    , subscriber_(nullptr)
 {
-    participant_.reset(
-        DomainParticipantFactory::get_instance()->create_participant(domain, qos),
-        [](eprosima::fastdds::dds::DomainParticipant* participant)
-        {
-            // deleter for shared ptr
-            if (!participant->delete_contained_entities())
-            {
-                logError(AMLIP_DDS_PARTICIPANT, "Fail deleting contained entities.");
-            }
+    // Set Participant name
+    qos.name(id.name());
 
-            if (!DomainParticipantFactory::get_instance()->delete_participant(participant))
-            {
-                logError(AMLIP_DDS_PARTICIPANT, "Fail deleting participant.");
-            }
-        }
-    );
-    if (nullptr == participant_)
-    {
-        throw ddsrouter::utils::InitializationException(
-            STR_ENTRY << "Failed to create participant " << id.name() << ".");
-    }
-
-    publisher_.reset(
-        participant_->create_publisher(default_publisher_qos_(), nullptr),
-        [this](eprosima::fastdds::dds::Publisher* publisher)
-        {
-            // deleter for shared ptr
-            if (!this->participant_->delete_publisher(publisher))
-            {
-                logError(AMLIP_DDS_PARTICIPANT, "Fail deleting publisher.");
-            }
-        }
-    );
-    if (nullptr == publisher_)
-    {
-        throw ddsrouter::utils::InitializationException(
-            STR_ENTRY << "Failed to create publisher in participant " << id.name() << ".");
-    }
-
-    subscriber_.reset(
-        participant_->create_subscriber(default_subscriber_qos_(), nullptr),
-        [this](eprosima::fastdds::dds::Subscriber* subscriber)
-        {
-            // deleter for shared ptr
-            if(!this->participant_->delete_subscriber(subscriber))
-            {
-                logError(AMLIP_DDS_PARTICIPANT, "Fail deleting subscriber.");
-            }
-        }
-    );
-    if (nullptr == subscriber_)
-    {
-        throw ddsrouter::utils::InitializationException(
-            STR_ENTRY << "Failed to create subscriber in participant " << id.name() << ".");
-    }
+    // Create DDS Handler
+    dds_handler_.reset(
+        new DdsHandler(qos, domain));
 }
 
 Participant::~Participant()
 {
-    // Destroy subentities before participant
-    topics_.clear();
-    types_.clear();
-    publisher_.reset();
-    subscriber_.reset();
-
-    // Destroy Participant
-    participant_.reset();
+    // Destroy DdsHandler that will destroy every DDS subentity
+    dds_handler_.reset();
 }
 
 types::AmlipId Participant::id() const noexcept
@@ -122,12 +56,12 @@ std::string Participant::name() const noexcept
     return id_.name();
 }
 
-eprosima::fastdds::dds::DomainParticipantQos Participant::default_participant_qos_() noexcept
+eprosima::fastdds::dds::DomainParticipantQos Participant::default_participant_qos() noexcept
 {
     return eprosima::fastdds::dds::PARTICIPANT_QOS_DEFAULT;
 }
 
-DomainIdType Participant::default_domain_id_() noexcept
+DomainIdType Participant::default_domain_id() noexcept
 {
     return DEFAULT_DOMAIN_ID_;
 }
