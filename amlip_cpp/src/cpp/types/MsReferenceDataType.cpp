12181660192cdda2a51359d9ae8947733e346974
jparisu <javierparis@eprosima.com>
2022-04-20 14:20:57 +0200

    Refs #14384: MultiService DataTypes
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ amlip_cpp/src/cpp/types/MsReferenceDataType.cpp
@@ -0,0 +1,221 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * @file MsReferenceDataType.cpp
+ */
+
+#include <fastcdr/Cdr.h>
+
+#include <fastcdr/exceptions/BadParamException.h>
+using namespace eprosima::fastcdr::exception;
+
+#include <algorithm>
+#include <array>
+#include <random>
+#include <string>
+#include <utility>
+
+#include <types/MsReferenceDataType.hpp>
+
+namespace eprosima {
+namespace amlip {
+namespace types {
+
+MsReferenceDataType::MsReferenceDataType(
+        const AmlipIdDataType source_id,
+        const TaskId& task_id,
+        const AmlipIdDataType& target_id)
+    : MsRequestDataType(source_id, task_id)
+    , target_id_(target_id)
+{
+}
+
+MsReferenceDataType::~MsReferenceDataType()
+{
+}
+
+MsReferenceDataType::MsReferenceDataType(
+        const MsReferenceDataType& x)
+{
+    source_id_ = x.source_id_;
+    task_id_ = x.task_id_;
+    target_id_ = x.target_id_;
+}
+
+MsReferenceDataType::MsReferenceDataType(
+        MsReferenceDataType&& x)
+{
+    source_id_ = std::move(x.source_id_);
+    task_id_ = std::move(x.task_id_);
+    target_id_ = std::move(x.target_id_);
+}
+
+MsReferenceDataType& MsReferenceDataType::operator =(
+        const MsReferenceDataType& x)
+{
+    source_id_ = x.source_id_;
+    task_id_ = x.task_id_;
+    target_id_ = x.target_id_;
+
+    return *this;
+}
+
+MsReferenceDataType& MsReferenceDataType::operator =(
+        MsReferenceDataType&& x)
+{
+    source_id_ = std::move(x.source_id_);
+    task_id_ = std::move(x.task_id_);
+    target_id_ = std::move(x.target_id_);
+
+    return *this;
+}
+
+bool MsReferenceDataType::operator ==(
+        const MsReferenceDataType& x) const
+{
+    return (source_id_ == x.source_id_ && task_id_ == x.task_id_ && target_id_ == x.target_id_);
+}
+
+bool MsReferenceDataType::operator !=(
+        const MsReferenceDataType& x) const
+{
+    return !(*this == x);
+}
+
+bool MsReferenceDataType::operator <(
+        const MsReferenceDataType& x) const
+{
+    if (source_id_ < x.source_id_)
+    {
+        return true;
+    }
+    else if (x.source_id_ < source_id_)
+    {
+        return false;
+    }
+    else
+    {
+        if (task_id_ < x.task_id_)
+        {
+            return true;
+        }
+        else if (x.task_id_ < task_id_)
+        {
+            return false;
+        }
+        else
+        {
+            return (target_id_ < x.target_id_);
+        }
+    }
+}
+
+AmlipIdDataType MsReferenceDataType::target_id() const
+{
+    return target_id_;
+}
+
+void MsReferenceDataType::target_id(const AmlipIdDataType& new_value)
+{
+    target_id_ = new_value;
+}
+
+void MsReferenceDataType::serialize(
+        eprosima::fastcdr::Cdr& scdr) const
+{
+    scdr << source_id_;
+    scdr << task_id_;
+    scdr << target_id_;
+}
+
+void MsReferenceDataType::deserialize(
+        eprosima::fastcdr::Cdr& dcdr)
+{
+    dcdr >> source_id_;
+    dcdr >> task_id_;
+    dcdr >> target_id_;
+}
+
+void MsReferenceDataType::serialize_key(
+        eprosima::fastcdr::Cdr&) const
+{
+}
+
+size_t MsReferenceDataType::get_max_cdr_serialize_size(
+        size_t current_alignment)
+{
+    size_t initial_alignment = current_alignment;
+
+    current_alignment += AmlipIdDataType::get_max_cdr_serialize_size(current_alignment);
+    current_alignment += 4 + eprosima::fastcdr::Cdr::alignment(current_alignment, 4);
+    current_alignment += AmlipIdDataType::get_max_cdr_serialize_size(current_alignment);
+
+    return current_alignment - initial_alignment;
+}
+
+size_t MsReferenceDataType::get_cdr_serialize_size(
+        const MsReferenceDataType& request,
+        size_t current_alignment)
+{
+    size_t initial_alignment = current_alignment;
+
+    current_alignment += AmlipIdDataType::get_cdr_serialize_size(request.source_id(), current_alignment);
+    current_alignment += 4 + eprosima::fastcdr::Cdr::alignment(current_alignment, 4);
+    current_alignment += AmlipIdDataType::get_cdr_serialize_size(request.target_id(), current_alignment);
+
+    return current_alignment - initial_alignment;
+}
+
+size_t MsReferenceDataType::get_key_max_cdr_serialized_size(
+        size_t current_alignment)
+{
+    return current_alignment;
+}
+
+bool MsReferenceDataType::is_key_defined()
+{
+    return false;
+}
+
+bool MsReferenceDataType::is_bounded()
+{
+    return true;
+}
+
+bool MsReferenceDataType::is_plain()
+{
+    return true;
+}
+
+bool MsReferenceDataType::construct_sample(
+        void* memory)
+{
+    new (memory) MsReferenceDataType();
+    return true;
+}
+
+std::ostream& operator <<(
+        std::ostream& os,
+        const MsReferenceDataType& request)
+{
+    os << "MS-REFERENCE{" << request.source_id() <<
+        "|" << request.task_id() <<
+        "|" << request.target_id() << "}";
+    return os;
+}
+
+} /* namespace types */
+} /* namespace amlip */
+} /* namespace eprosima */
---
+++ amlip_cpp/src/cpp/types/MsReferenceDataType.hpp
@@ -0,0 +1,219 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * @file MsReferenceDataType.hpp
+ */
+
+#ifndef AMLIP__SRC_CPP_TYPES_MSREFERENCEDATATYPE_HPP
+#define AMLIP__SRC_CPP_TYPES_MSREFERENCEDATATYPE_HPP
+
+#include <array>
+#include <limits>
+#include <ostream>
+#include <string>
+
+#include <types/AmlipIdDataType.hpp>
+#include <types/MsRequestDataType.hpp>
+
+namespace eprosima {
+namespace fastcdr {
+class Cdr;
+} // namespace fastcdr
+} // namespace eprosima
+
+namespace eprosima {
+namespace amlip {
+namespace types {
+
+/*!
+ * @brief This class represents the structure MsReferenceDataType, which implements \c InterfaceDataType and hence serves
+ * as a communication interface to \c AmlipId class.
+ * @ingroup AMLIP
+ */
+class MsReferenceDataType : public MsRequestDataType
+{
+public:
+
+    /**
+     * TODO
+     */
+    MsReferenceDataType();
+
+    /*!
+     * @brief Constructor with name.
+     */
+    MsReferenceDataType(
+            const AmlipIdDataType source_id,
+            const TaskId& task_id,
+            const AmlipIdDataType& target_id);
+
+    /*!
+     * @brief Default destructor.
+     */
+    ~MsReferenceDataType();
+
+    /*!
+     * @brief Copy constructor.
+     * @param x Reference to the object MsReferenceDataType that will be copied.
+     */
+    MsReferenceDataType(
+            const MsReferenceDataType& x);
+
+    /*!
+     * @brief Move constructor.
+     * @param x Reference to the object MsReferenceDataType that will be copied.
+     */
+    MsReferenceDataType(
+            MsReferenceDataType&& x);
+
+    /*!
+     * @brief Copy assignment.
+     * @param x Reference to the object MsReferenceDataType that will be copied.
+     */
+    MsReferenceDataType& operator =(
+            const MsReferenceDataType& x);
+
+    /*!
+     * @brief Move assignment.
+     * @param x Reference to the object MsReferenceDataType that will be copied.
+     */
+    MsReferenceDataType& operator =(
+            MsReferenceDataType&& x);
+
+    /*!
+     * @brief Comparison operator.
+     * @param x MsReferenceDataType object to compare.
+     */
+    bool operator ==(
+            const MsReferenceDataType& x) const;
+
+    /*!
+     * @brief Comparison operator.
+     * @param x MsReferenceDataType object to compare.
+     */
+    bool operator !=(
+            const MsReferenceDataType& x) const;
+
+    /*!
+     * @brief Comparison operator.
+     * @param x MsReferenceDataType object to compare.
+     */
+    bool operator <(
+            const MsReferenceDataType& x) const;
+
+    /*!
+     * TODO
+     */
+    AmlipIdDataType target_id() const;
+
+    /*!
+     * TODO
+     */
+    void target_id(const AmlipIdDataType& new_value);
+
+    /////
+    // InterfaceDataType methods
+
+    /*!
+     * @brief This function serializes an object using CDR serialization.
+     * @param cdr CDR serialization object.
+     */
+    void serialize(
+            eprosima::fastcdr::Cdr& cdr) const override;
+
+    /*!
+     * @brief This function deserializes an object using CDR serialization.
+     * @param cdr CDR serialization object.
+     */
+    void deserialize(
+            eprosima::fastcdr::Cdr& cdr) override;
+
+    /*!
+     * @brief This function serializes the key members of an object using CDR serialization.
+     * @param cdr CDR serialization object.
+     */
+    void serialize_key(
+            eprosima::fastcdr::Cdr& cdr) const override;
+
+    /*!
+     * @brief This function returns the maximum serialized size of an object
+     * depending on the buffer alignment.
+     * @param current_alignment Buffer alignment.
+     * @return Maximum serialized size.
+     */
+    static size_t get_max_cdr_serialize_size(
+            size_t current_alignment = 0);
+
+    /*!
+     * @brief This function returns the serialized size of a data depending on the buffer alignment.
+     * @param data Data which is calculated its serialized size.
+     * @param current_alignment Buffer alignment.
+     * @return Serialized size.
+     */
+    static size_t get_cdr_serialize_size(
+            const MsReferenceDataType& data,
+            size_t current_alignment = 0);
+
+    /*!
+     * @brief This function returns the maximum serialized size of the Key of an object
+     * depending on the buffer alignment.
+     * @param current_alignment Buffer alignment.
+     * @return Maximum serialized size.
+     */
+    static size_t get_key_max_cdr_serialized_size(
+            size_t current_alignment = 0);
+
+    /*!
+     * @brief This function tells you if the Key has been defined for this type
+     */
+    static bool is_key_defined();
+
+    /**
+     * @brief Whether the type is bounded
+     */
+    static bool is_bounded();
+
+    /**
+     * @brief Whether the type is plain
+     */
+    static bool is_plain();
+
+    /**
+     * @brief Construct an empty sample in the memory allocated
+     *
+     * @pre The type must be plain
+     *
+     * @param memory already allocated memory for the new data
+     *
+     * @return true if the construction was successful, false otherwise
+     */
+    static bool construct_sample(
+            void* memory);
+
+protected:
+
+    AmlipIdDataType target_id_;
+};
+
+//! \c MsReferenceDataType to stream serializator
+std::ostream& operator <<(
+        std::ostream& os,
+        const MsReferenceDataType& request);
+
+} /* namespace types */
+} /* namespace amlip */
+} /* namespace eprosima */
+
+#endif // AMLIP__SRC_CPP_TYPES_MSREFERENCEDATATYPE_HPP
