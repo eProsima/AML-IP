6ea0828e063f14c161d3d68ecaaf3883b8092d2e
jparisu <javierparis@eprosima.com>
2022-05-13 09:42:32 +0200

    Refs #14549: Implement SWIG binding library
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ amlip_py/project_settings.cmake
@@ -0,0 +1,33 @@
+# Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###############################################################################
+# Set settings for project amlip_py for SWIG code
+###############################################################################
+
+set(MODULE_NAME
+    amlip_swig)
+
+set(MODULE_SUMMARY
+    "SWIG library to create python bindings from amlip_cpp.")
+
+set(MODULE_FIND_PACKAGES
+        amlip_cpp
+        ddsrouter_utils
+        ddsrouter_event
+    )
+
+set(MODULE_DEPENDENCIES
+        ${MODULE_FIND_PACKAGES}
+    )
