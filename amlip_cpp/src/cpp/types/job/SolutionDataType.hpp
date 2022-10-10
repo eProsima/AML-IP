b4b6a414f9ba8219dd561c128f0cd877e62b5a02
jparisu <69341543+jparisu@users.noreply.github.com>
2022-10-10 07:56:52 +0200

    Job & Solution DataType (#41)
    * Refs #14424: Job and Solution Headers
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #14424: apply suggestions
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

---
+++ amlip_cpp/src/cpp/types/job/SolutionDataType.hpp
@@ -0,0 +1,46 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * @file SolutionDataType.hpp
+ */
+
+#ifndef AMLIPCPP__SRC_CPP_TYPES_SOLUTIONDATATYPE_HPP
+#define AMLIPCPP__SRC_CPP_TYPES_SOLUTIONDATATYPE_HPP
+
+#include <types/GenericDataType.hpp>
+
+namespace eprosima {
+namespace amlip {
+namespace types {
+
+/*!
+ * @brief AML Solution Task Data Type
+ *
+ * This class may be reimplemented according with AML team designs.
+ */
+class SolutionDataType : public GenericDataType
+{
+public:
+
+    //! Use parent constructors
+    using GenericDataType::GenericDataType;
+
+};
+
+} /* namespace types */
+} /* namespace amlip */
+} /* namespace eprosima */
+
+#endif // AMLIPCPP__SRC_CPP_TYPES_SOLUTIONDATATYPE_HPP
