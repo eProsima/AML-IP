e3e438941e4bf9c37785fbe2112acb02a03a3ca0
Irene Bandera <irenebandera@eprosima.com>
2023-02-09 15:59:01 +0100

    Implement Inference Node
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

---
+++ amlip_cpp/include/amlip_cpp/node/InferenceNode.hpp
@@ -0,0 +1,161 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file InferenceNode.hpp
+ */
+
+#ifndef AMLIPCPP__SRC_CPP_NODE_INFERENCENODE_HPP
+#define AMLIPCPP__SRC_CPP_NODE_INFERENCENODE_HPP
+
+#include <functional>
+
+#include <amlip_cpp/node/ParentNode.hpp>
+#include <amlip_cpp/types/inference/InferenceDataType.hpp>
+#include <amlip_cpp/types/inference/InferenceSolutionDataType.hpp>
+
+// Forward declaration of dds classes
+namespace eprosima {
+namespace amlip {
+namespace dds {
+
+template <typename Task, typename Solution>
+class MultiServiceServer;
+
+} /* namespace dds */
+} /* namespace amlip */
+} /* namespace eprosima */
+
+namespace eprosima {
+namespace amlip {
+namespace node {
+
+/**
+ * @brief Object that is called when a new Inference data has been received.
+ *
+ * This class is supposed to be implemented by a User and be given to a \c InferenceNode in order to process a Inference.
+ * When a Inference is received, \c process_inference is called and it is expected to return a Solution for such inference.
+ */
+class AMLIP_CPP_DllAPI InferenceListener
+{
+public:
+
+    //! Default virtual dtor so it can be inherited.
+    virtual ~InferenceListener() = default;
+
+    /**
+     * @brief Method that will be called with the Inference message received
+     *
+     * @param inference new Inference message received.
+     *
+     * @return Solution to the \c inference .
+     */
+    virtual types::InferenceSolutionDataType process_inference (
+            const types::InferenceDataType& inference) const = 0;
+};
+
+/**
+ * @brief This is a specialization of AML-IP Node that waits for Inference data and retrieves a Solution.
+ *
+ * Inference Nodes are the ones in charge of receiving training data from a Main Node
+ * Using \c process_inference will wait for a Main Node to send it the data for a Inference, and will process this Inference by
+ * the Listener or callback given, and return the Solution calculated.
+ *
+ * @todo implement an asynchronous request_inference_solution method.
+ *
+ * @warning Not Thread Safe (yet) (TODO)
+ */
+class InferenceNode : public ParentNode
+{
+public:
+
+    /**
+     * @brief Construct a new Inference Node object.
+     *
+     * @param name name of the Node (it is advisable to be unique, or at least representative).
+     */
+    AMLIP_CPP_DllAPI InferenceNode(
+            const char* name);
+
+    //! Same as previous ctor but with a string argument.
+    AMLIP_CPP_DllAPI InferenceNode(
+            const std::string& name);
+
+    /**
+     * @brief Destroy the Main Node object and its internal DDS entities.
+     *
+     * @pre Cannot be destroyed while processing a inference. Otherwise undefined behaviour.
+     */
+    AMLIP_CPP_DllAPI ~InferenceNode();
+
+    /**
+     * @brief Wait for a Inference to be received and give a solution by \c callback .
+     *
+     * This sets the status of this Node as available to receive Inferences, and waits for a Main Node to ask for a Inference.
+     * Once the MultiService handshake has been done with a Main Node, it will send a Inference data to this Node and it will
+     * be resolved by \c callback , that must give the \c InferenceSolutionDataType for the inference.
+     *
+     * @attention this method is synchronous and will not finish until the inference has been solved.
+     *
+     * @todo asynchronous mode
+     *
+     * @param callback [in] function that receives a \c InferenceDataType and returns a \c InferenceSolutionDataType .
+     * @param client_id [out] Id of the client that sent the Inference.
+     */
+
+    AMLIP_CPP_DllAPI void process_inference(
+            const std::function<types::InferenceSolutionDataType(const types::InferenceDataType&)>& callback,
+            types::AmlipIdDataType& client_id);
+
+    //! Same as previous \c process_inference without client_id return parameter.
+    AMLIP_CPP_DllAPI void process_inference(
+            const std::function<types::InferenceSolutionDataType(const types::InferenceDataType&)>& callback);
+
+    /**
+     * @brief Wait for a Inference to be received and give a solution by \c listener \c process_inference method .
+     *
+     * This sets the status of this Node as available to receive Inferences, and waits for a Main Node to ask for a Inference.
+     * Once the MultiService handshake has been done with a Main Node, it will send a Inference data to this Node and it will
+     * be resolved by calling \c process_inference of \c listener , that must give the \c InferenceSolutionDataType for the inference.
+     *
+     * @attention this method is synchronous and will not finish until the inference has been solved.
+     *
+     * @todo asynchronous mode
+     *
+     * @param listener [in] listener to call with a \c InferenceDataType and returns a \c InferenceSolutionDataType .
+     * @param client_id [out] Id of the client that sent the Inference.
+     */
+    AMLIP_CPP_DllAPI void process_inference(
+            const InferenceListener& listener,
+            types::AmlipIdDataType& client_id);
+
+    //! Same as previous \c process_inference without client_id return parameter.
+    AMLIP_CPP_DllAPI void process_inference(
+            const InferenceListener& listener);
+
+protected:
+
+    /**
+     * @brief Reference to the MultiService Server that sends inferences.
+     *
+     * This is created from DDS Participant in ParentNode, and its destruction is handled by ParentNode.
+     */
+    std::shared_ptr<dds::MultiServiceServer<types::InferenceDataType, types::InferenceSolutionDataType>> inference_server_;
+};
+
+} /* namespace node */
+} /* namespace amlip */
+} /* namespace eprosima */
+
+#endif /* AMLIPCPP__SRC_CPP_NODE_INFERENCENODE_HPP */
---
+++ amlip_cpp/src/cpp/node/InferenceNode.cpp
@@ -0,0 +1,103 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file InferenceNode.cpp
+ */
+
+#include <cpp_utils/Log.hpp>
+#include <cpp_utils/exception/InconsistencyException.hpp>
+
+#include <amlip_cpp/node/InferenceNode.hpp>
+
+#include <dds/multiservice/MultiServiceServer.hpp>
+#include <dds/Participant.hpp>
+#include <dds/network_utils/topic.hpp>
+
+namespace eprosima {
+namespace amlip {
+namespace node {
+
+InferenceNode::InferenceNode(
+        const char* name)
+    : ParentNode(name, types::NodeKind::inference)
+    , inference_server_(participant_->create_multiservice_server<types::InferenceDataType, types::InferenceSolutionDataType>(
+                dds::utils::INFERENCE_TOPIC_NAME))
+{
+    logInfo(AMLIPCPP_NODE_INFERENCE, "Created new Inference Node: " << *this << ".");
+}
+
+InferenceNode::InferenceNode(
+        const std::string& name)
+    : InferenceNode(name.c_str())
+{
+}
+
+InferenceNode::~InferenceNode()
+{
+    logDebug(AMLIPCPP_NODE_INFERENCE, "Destroying Inference Node: " << *this << ".");
+}
+
+void InferenceNode::process_inference(
+        const std::function<types::InferenceSolutionDataType(const types::InferenceDataType&)>& callback)
+{
+    types::AmlipIdDataType _;
+    process_inference(callback, _);
+}
+
+void InferenceNode::process_inference(
+        const std::function<types::InferenceSolutionDataType(const types::InferenceDataType&)>& callback,
+        types::AmlipIdDataType& client_id)
+{
+    change_status_(types::StateKind::running);
+    auto reference = inference_server_->process_task_sync(callback);
+    client_id = reference.client_id();
+    change_status_(types::StateKind::stopped);
+}
+
+void InferenceNode::process_inference(
+        const InferenceListener& listener)
+{
+    process_inference(
+        [&listener]
+            (const types::InferenceDataType& inference)
+        {
+            return listener.process_inference(inference);
+        });
+}
+
+void InferenceNode::process_inference(
+        const InferenceListener& listener,
+        types::AmlipIdDataType& client_id)
+{
+    process_inference(
+        [&listener]
+            (const types::InferenceDataType& inference)
+        {
+            return listener.process_inference(inference);
+        },
+        client_id);
+}
+
+std::ostream& operator <<(
+        std::ostream& os,
+        const InferenceNode& node)
+{
+    os << "INFERENCE_NODE{" << node.id() << "}";
+    return os;
+}
+
+} /* namespace node */
+} /* namespace amlip */
+} /* namespace eprosima */
