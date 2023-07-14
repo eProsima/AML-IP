d23e75077f528f3c9c41cb9088c9f7f4d6ae77ac
Irene Bandera <irenebandera@eprosima.com>
2023-07-14 10:07:29 +0200

    Add blackbox test
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- amlip_py/test/amlip_py/node/blackbox/test_modelReceiverSenderNodeTest.py
+++ amlip_py/test/amlip_py/node/blackbox/test_modelReceiverSenderNodeTest.py
@@ -14,6 +14,7 @@
 
 from py_utils.wait.BooleanWaitHandler import BooleanWaitHandler
 
+from amlip_py.node.ModelManagerSenderNode import ModelManagerSenderNode, ModelReplier
 from amlip_py.node.ModelManagerReceiverNode import ModelManagerReceiverNode, ModelListener
 from amlip_py.types.AmlipIdDataType import AmlipIdDataType
 from amlip_py.types.ModelDataType import ModelDataType
@@ -23,20 +24,36 @@ from amlip_py.types.ModelStatisticsDataType import ModelStatisticsDataType
 # Domain ID
 DOMAIN_ID = 166
 
-# Variable to wait to the model reply
+# Variable to wait to the model request
 waiter = BooleanWaitHandler(True, False)
 
+
+class CustomModelReplier(ModelReplier):
+
+    def fetch_model(
+            self,
+            model: ModelDataType) -> ModelSolutionDataType:
+
+        solution = ModelSolutionDataType(model.to_string().upper())
+
+        print(f'Model request received from client\n'
+              f' model: {model.to_string()}\n'
+              f' solution: {solution.to_string()}')
+
+        return solution
 
 class CustomModelListener(ModelListener):
 
     def statistics_received(
             self,
             statistics: ModelStatisticsDataType) -> bool:
+
         return True
 
     def model_received(
             self,
             model: ModelSolutionDataType) -> bool:
+
         print(f'Model reply received from server\n'
               f' solution: {model.to_string()}')
 
@@ -44,35 +61,53 @@ class CustomModelListener(ModelListener):
 
         return True
 
-
 def main():
     """Execute main routine."""
 
     # Create request
     data = ModelDataType('MobileNet V1')
 
-    id = AmlipIdDataType('ModelManagerReceiver')
-    id.set_id([66, 66, 66, 66])
+    id_receiver = AmlipIdDataType('ModelManagerReceiver')
+    id_receiver.set_id([66, 66, 66, 66])
 
-    # Create node
+    id_sender = AmlipIdDataType('ModelManagerSender')
+    id_sender.set_id([66, 66, 66, 66])
+
+    # Create nodes
     print('Starting Manual Test Model Manager Receiver Node Py execution. Creating Node...')
     model_receiver_node = ModelManagerReceiverNode(
-        id=id,
+        id=id_receiver,
         data=data,
         domain=DOMAIN_ID)
 
     print(f'Node created: {model_receiver_node.get_id()}. '
           'Already processing models.')
 
+    print('Starting Manual Test Model Manager Sender Node Py execution. Creating Node...')
+    model_sender_node = ModelManagerSenderNode(
+        id=id_sender,
+        domain=DOMAIN_ID)
+
+    print(f'Node created: {model_sender_node.get_id()}. '
+          'Already processing models.')
+
+    model_sender_node.update_statistics(
+        'ModelManagerSenderStatistics',
+        'hello world')
+
     model_receiver_node.start(
         listener=CustomModelListener())
 
-    # Wait for reply
+    model_sender_node.start(
+        listener=CustomModelReplier())
+
+    # Wait for the solution to be sent
     waiter.wait()
 
     model_receiver_node.stop()
+    model_sender_node.stop()
 
-    print('Finishing Manual Test Model Manager Receiver Node Py execution.')
+    print('Finishing Manual Test Model Manager Sender Node Py execution.')
 
 
 # Call main in program execution
